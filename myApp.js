f4de9c52396a89e03982e49a5f77413a019b68b8
ymk <1766273199@qq.com>
2024-08-03 02:07:14 +0800

    finish

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
+app.use(bodyParser.urlencoded({extended: true}))
+require('dotenv').config()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
